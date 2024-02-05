0edb2de209736b8a0895d7e63b00a6f882ef4b3a
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-02-05 16:16:15 -0300

    feat: add Serviceitem component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import { Covered_By_Your_Grace } from "next/font/google";
+import Image from "next/image";
+import BarbershopInfo from "./_components/barbershop-info";
+import ServiceItem from "./_components/service-item";
+import { Key } from "react";
+
+interface BabershopDetailsPageProps{
+    params: {
+        id?: string;
+    }
+}
+
+const BabershopDetailsPage = async ({params}:BabershopDetailsPageProps) => {
+    if (!params.id){
+        return null;
+    }
+
+    const barbershop = await db.barbershop.findUnique({
+        where:{
+            id: params.id,
+        },
+        include:{
+            services: true,
+        }
+    });
+
+    if (!barbershop){
+        return null;
+    }
+    
+    return ( 
+        <div>
+            <BarbershopInfo barbershop={barbershop} />
+
+            <div className="px-5 flex flex-col gap-4 py-6">
+                {barbershop.services.map((service: { id: Key | null | undefined; }) => (
+                    <ServiceItem key={service.id} service={service}/>
+                ))}
+            </div>
+        </div>
+     );
+}
+ 
+export default BabershopDetailsPage;
