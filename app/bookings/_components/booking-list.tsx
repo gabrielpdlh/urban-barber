24937443c562fcb5c531f9c36d1d60aa10b3ed9c
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-02-15 19:52:25 -0300

    feat: add bookings page

---
+++ app/bookings/_components/booking-list.tsx
@@ -0,0 +1,14 @@
+"use client"
+
+import { Booking } from "@prisma/client";
+import { Heading1 } from "lucide-react";
+
+interface BookingListProps{
+    booking: Booking
+}
+
+const BookingList = () => {
+    return (  Heading1);
+}
+ 
+export default BookingList;
