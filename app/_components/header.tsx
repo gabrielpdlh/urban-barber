e4ccc9afdf5af0234a1c1a41a5110b1a01519890
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-02-16 22:37:54 -0300

    fix: logo

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -20,7 +20,7 @@ const Header = () => {
     return ( 
         <Card>
             <CardContent className="p-5 flex justify-between items-center">
-                <Link href={'/'}><Image src="/logo.png" alt="FSW Barber" height={22} width={120}/></Link>
+                <Link href={'/'}><Image src="/Logo.png" alt="FSW Barber" height={22} width={120}/></Link>
                 <Sheet>
                     <SheetTrigger asChild>
                         <Button variant="outline" size="icon">
