e3ed5491186b578f8f52069ca6c958a438bc0784
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-02-16 21:04:59 -0300

    test

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,6 @@ import { authOptions } from "@/app/_lib/auth"
 import NextAuth from "next-auth"
 
 
-
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST, authOptions }
