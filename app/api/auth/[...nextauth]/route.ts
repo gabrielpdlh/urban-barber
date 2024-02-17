a49b0500338bb867c90bdf9b9e3c39a84114abf2
Gabriel Padilha <gabriel.padilha1204@gmail.com>
2024-02-16 21:23:46 -0300

    fix: import error

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
-import { authOptions } from "@/app/_lib/auth"
-import NextAuth from "next-auth"
+import { authOptions } from "@/app/_lib/auth";
+import NextAuth from "next-auth";
 
+const handler = NextAuth(authOptions);
 
-const handler = NextAuth(authOptions)
-
-export { handler as GET, handler as POST, authOptions }
+export { handler as GET, handler as POST };
